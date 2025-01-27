eb76611219ed0f0bc15eac73ecfa99e989e3673f
elisabichiappini <elisabichiappini@gmail.com>
2025-01-27 14:19:06 +0100

    feat: add rotta create

--- routers/pizze.js
+++ routers/pizze.js
@@ -5,6 +5,12 @@ const router = express.Router();
 const pizzeController = require('../controllers/pizze.js');
 //router metodo che apre la rotta utilizzando il controller x ( qui siamo già in /pizze)
 router.get('/', pizzeController.index);
+
+//importante la posizione sotto il get /
+router.get('/create', (req, res) =>{
+    res.send('<h2>crea una nuova pizza</h2>');
+});
+
 //qui siamo in /pizze/:slug
 router.get('/:slug', pizzeController.show);
 
